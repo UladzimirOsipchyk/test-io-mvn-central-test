9360cc48473b9521715f130efebc175c424719fe
Uladzimir_Osipchyk <uladzimir_osipchyk@epam.com>
2022-07-12 12:21:57 +0400

    init commit

---
+++ src/main/java/com/uoo/example/testiomvncentraltestapplication/TestIoMvnCentralTestApplication.java
@@ -0,0 +1,13 @@
+package com.uoo.example.testiomvncentraltestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIoMvnCentralTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestIoMvnCentralTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/uoo/example/testiomvncentraltestapplication/TestIoMvnCentralTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoo.example.testiomvncentraltestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIoMvnCentralTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
